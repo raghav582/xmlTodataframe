85c60c7699d4f5204496f9913496a7c3fb9d2534
Raghav Sharma <raghavsharma582@gmail.com>
2020-02-25 15:32:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from distutils.core import setup
 setup(
   name = 'xmlTodataframe',
   packages = ['xml2dataframe'],
-  version = '0.1',
+  version = '0.2',
   license='GPL-3.0-only',
   description = 'xmlTodataframe convert given xml file to csv file!',
   author = 'Raghav Sharma',
